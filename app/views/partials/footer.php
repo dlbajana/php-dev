219448553ad3264b5328ede6e4f0dab27fd56421
Daniel Bajana <it.danielbajana@gmail.com>
2018-06-25 22:05:17 +0800

    Add jquery and popper cdn reference

--- app/views/partials/footer.php
+++ app/views/partials/footer.php
@@ -1,2 +1,5 @@
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+    <script src="/public/js/bootstrap.min.js"></script>
 </body>
 </html>
--- app/views/partials/head.php
+++ app/views/partials/head.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <link rel="stylesheet" href="/public/css/bootstrap.min.css">
 </head>
 <body>
     <?php require('nav.php'); ?>
